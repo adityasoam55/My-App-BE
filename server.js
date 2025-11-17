47774591f86721bd8c5138c2c8ccfb38f5deab7b
Aditya som <ADITYASOAM55@GMAIL.COM>
2025-11-17 13:28:55 +0530

    CORS code updated with backend url

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const authRoutes = require("./routes/auth");
-const taskRoutes = require('./routes/tasks')
+const taskRoutes = require("./routes/tasks");
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: true })); // Allow front-end origin
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://my-app-fe.netlify.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Backend working!");
@@ -21,7 +27,7 @@ mongoose
   .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use("/api/auth", authRoutes);
-app.use('/api/tasks', taskRoutes);
+app.use("/api/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
